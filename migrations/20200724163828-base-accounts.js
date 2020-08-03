cd0780bcfb8c425a67866988411c477eb645e94b
Murod Khaydarov <murod.haydarov@gmail.com>
2020-08-03 20:31:02 +0300

    migration file comments

--- migrations/20200724163828-base-accounts.js
+++ migrations/20200724163828-base-accounts.js
@@ -1,24 +1,47 @@
+/**
+ * Migration creates base accounting accounts such as Cashbook and revenue
+ */
 module.exports = {
+  /**
+   * Create Cashbook and Revenue accounts with USD currency
+   *
+   * @param db
+   * @param client
+   * @returns {Promise<void>}
+   */
   async up(db, client) {
     const cashbook = {
-      id: "22354b8a-b501-44ed-bff0-d3bd8d899dbf",
-      name: "Hawk cashbook",
+      id: '22354b8a-b501-44ed-bff0-d3bd8d899dbf',
+      name: 'Hawk cashbook',
       type: 'Asset',
-      currency: 'USD'
+      currency: 'USD',
     };
 
     const revenue = {
-      id: "953fb6fd-5a86-4c34-9587-abd493b1175e",
-      name: "Hawk revenue",
+      id: '953fb6fd-5a86-4c34-9587-abd493b1175e',
+      name: 'Hawk revenue',
       type: 'Revenue',
-      currency: 'USD'
+      currency: 'USD',
     };
 
     db.collection('accounts').insertOne(cashbook);
     db.collection('accounts').insertOne(revenue);
   },
 
+  /**
+   * Rollback account creation
+   *
+   * @param db
+   * @param client
+   * @returns {Promise<void>}
+   */
   async down(db, client) {
-    // db.collection('accounts').remove
-  }
+    db.collection('accounts').remove({
+      id: '22354b8a-b501-44ed-bff0-d3bd8d899dbf',
+    });
+
+    db.collection('accounts').remove({
+      id: '953fb6fd-5a86-4c34-9587-abd493b1175e',
+    });
+  },
 };
